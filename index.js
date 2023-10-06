eb14a2a5d1e6001ce70e5e207f97b81a20d53fe2
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-06 13:01:41 +0200

    Task 1 completed and started task 2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmarkCardButton"]');
+const showHideAnswerButton = document.querySelector(
+  '[data-js="showHideAnswerButton"]'
+);
+const answerText = document.querySelector('[data-js="answerText"]');
+
+//Toggle the bookmark Button
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+showHideAnswerButton.addEventListener("click", () => {
+  answerText.classList.toggle("card__answer--active");
+  if (answerText.classList.contains("card__answer--active")) {
+    showHideAnswerButton.textContent = "Hide answer";
+  } else {
+    showHideAnswerButton.textContent = "Show answer";
+  }
+});
